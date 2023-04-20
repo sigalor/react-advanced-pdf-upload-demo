7c656ed7ee8e8d3f5230a148e4d701054a578617
sigalor <nalenz@protonmail.com>
2023-04-20 09:47:57 +0200

    refactor: use Vite instead of create-react-app, use React 18

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'sanitize.css';
-import 'react-toastify/dist/ReactToastify.css';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
